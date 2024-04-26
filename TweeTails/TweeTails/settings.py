b3d4d6ec8bb4fa3cbde8e38f97a10b2ea5d561f7
jennymyzhang <j2466zha@uwaterloo.ca>
2024-04-26 18:29:15 -0400

    deploy

--- TweeTails/TweeTails/settings.py
+++ TweeTails/TweeTails/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9n6cbjs413oz!t1+#c*r$%t+0h=qmizcw!!f&_g%^kj6v45%kx'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOST").split(" ")
 
 
 # Application definition
@@ -134,7 +134,8 @@ DATABASES = {
     }
 }
 
-DATABASES["default"] = dj_database_url.parse("postgres://tweetails_user:MPYSRjXqXaMzPhK7fcqnZ5QkrNmD8IC9@dpg-com2cgol5elc73bsr7fg-a.ohio-postgres.render.com/tweetails")
+database_url = os.environ.get("DATABASE_URL")
+DATABASES["default"] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
