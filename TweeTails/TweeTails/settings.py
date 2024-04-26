08e4d0f7370cd7cd4839c0ca801a9ec87da060e8
jennymyzhang <j2466zha@uwaterloo.ca>
2024-04-26 19:03:54 -0400

    deploy

--- TweeTails/TweeTails/settings.py
+++ TweeTails/TweeTails/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOST").split(" ")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
